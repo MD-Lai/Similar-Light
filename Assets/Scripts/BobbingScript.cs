f9dd76c536fa9beca2d5ccebbd827dc7e50d38b5
MD-Lai <marvin.dhlai@gmail.com>
2017-11-28 22:29:01 +1100

    a 3d roamer inspired by hyperlight

---
+++ Assets/Scripts/BobbingScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobbingScript : MonoBehaviour {
+
+    public float bobAmp = 0.1f; // how many units to displace from centre
+    public float bobFreq = 0.5f; // how often to bob, in hz
+    public float offset = 0.5f; // how high the object should be above the given point
+    public float returnRate = 0.5f; // how long it takes to return to neutral
+
+    private float referenceTime;
+
+    void Start() {
+        referenceTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Bob();
+	}
+
+    private void Bob() {
+        Vector3 newPos = this.transform.position;
+        if (!MovementKeyPressed()) {
+
+            newPos.y = Mathf.Sin((Time.time-referenceTime) * Mathf.PI * bobFreq) * bobAmp + offset;
+
+        }
+        else {
+            newPos.y = Mathf.Lerp(this.transform.position.y, offset, Time.deltaTime / returnRate);
+        }
+
+        this.transform.position = newPos;
+    }
+
+    private bool MovementKeyPressed() {
+        //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) {
+        //    referenceTime = Time.time;
+        //    return true;
+        //}
+        //return false;
+        return false;
+    }
+}
---
+++ Assets/Scripts/FlyOverScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyOverScript : MonoBehaviour {
+
+    public float moveSpeed = 1; // units per second
+    public Vector3 moveAngle = new Vector3(0,45,0); //  angle to rotate about
+
+	// Use this for initialization
+	void Start () {
+		
+	}   
+	
+	// Update is called once per frame
+	void Update () {
+        InputMovement();
+	}
+
+    private void InputMovement() {
+        Vector3 forwardMove = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) {
+            forwardMove += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            forwardMove += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            forwardMove += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            forwardMove += Vector3.right;
+        }
+
+        this.transform.position += Quaternion.Euler(moveAngle.x, moveAngle.y, moveAngle.z) * forwardMove.normalized * moveSpeed * Time.deltaTime;
+    }
+}
